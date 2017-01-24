24a8eba09b23054bea5fc43f5d0c6b93968f792f
Karim Alibhai <karim@alibhai.co>
2017-01-23 20:01:32 -0500

    Add ssl csr config inline

--- ssl/gen.sh
+++ ssl/gen.sh
@@ -4,7 +4,7 @@
 openssl genrsa -out server.key 4096
 
 # generate signing request
-openssl req -new -sha256 -key server.key -out server.csr
+openssl req -new -sha256 -key server.key -out server.csr -subj '/C=CA/ST=Ontario/L=Ottawa/O=PLADO Inc./CN=secretary.plado.ca/emailAddress=support@plado.ca'
 
 # remove passphrase from key
 mv server.key server.key.org
